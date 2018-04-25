4aa3fd0b9432427d4fb32288cb9ca02061e7ac48
sachin jamthe <>
2018-04-24 23:32:38 -0500

    First Commit

---
+++ table.js
@@ -0,0 +1,15 @@
+//Callback code
+
+//function to prepare food
+
+function prepareFood(tableNumber, items, callback){
+
+    console.log("preparing food for items - > (" + tableNumber  + ") " + items);
+    callback(tableNumber, items);
+}
+// function to serve food
+function serveFood(tableNumber, items){
+    console.log("serving food for items - > (" + tableNumber  + ") " + items);
+}
+//calling prepare food and pass callback function
+prepareFood(3, ["Burger", "Pasta", "Coke"], serveFood);
